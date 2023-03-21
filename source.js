22d46258857e0c4c14ae47e1b56b0e4c4dafef98
hazdawg11 <40917470+hazdawg11@users.noreply.github.com>
2023-03-22 09:52:56 +1100

    Add files via upload

---
+++ source.js
@@ -0,0 +1,61 @@
+var bookmarks = new Array();
+bookmarks[0] = new Array(
+	'YouTube',
+	'http://gamespot.com',
+	'youtube.png'
+);
+bookmarks[1] = new Array(
+	'Yahoo',
+	'https://discordapp.com/channels/@me',
+	'yahoo.png'
+);
+bookmarks[2] = new Array(
+	'Grooveshark',
+	'http://ign.com/',
+	'grooveshark.png'
+);
+bookmarks[3] = new Array(
+	'last.fm',
+	'https://messenger.com',
+	'lastfm.png'
+);
+bookmarks[4] = new Array(
+	'twitter',
+	'http://twitter.com',
+	'twitter.png'
+);
+bookmarks[5] = new Array(
+	'google',
+	'http://google.com',
+	'google.png'
+);
+bookmarks[6] = new Array(
+	'facebook',
+	'https://www.facebook.com/?ref=logo',
+	'facebook.png'
+);
+bookmarks[7] = new Array(
+	'BBC news',
+	'https://www.twitch.tv/kindafunnygames',
+	'bbcnews.png'
+);
+bookmarks[8] = new Array(
+	'CNN',
+	'https://press-start.com.au/category/news/',
+	'cnn.png'
+);
+bookmarks[9] = new Array(
+	'deviantART',
+	'http://www.youtube.com/feed/subscriptions/u',
+	'deviantart.png'
+);
+bookmarks[10] = new Array(
+	'wikipedia',
+	'http://www.soundcloud.com/dashboard/incoming',
+	'wikipedia.png'
+);
+bookmarks[11] = new Array(
+	'iTunes',
+	'https://www.reddit.com/',
+	'itunes.png'
+);
